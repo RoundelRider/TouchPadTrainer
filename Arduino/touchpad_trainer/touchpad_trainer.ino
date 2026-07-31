ee9f3d2b2e17fc333175a776182bc76a69e5111b
Golden Murray <golden.murray@brighthorizons.com>
2026-07-30 19:31:56 -0600

    Using CAP1188 touch sensor boards

--- Arduino/touchpad_trainer/touchpad_trainer.ino
+++ Arduino/touchpad_trainer/touchpad_trainer.ino
@@ -22,9 +22,30 @@
  */
 
 #include <Wire.h>
-#include <Adafruit_MPR121.h>
+//#include <Adafruit_MPR121.h>
+#include <SPI.h>
+#include <Adafruit_CAP1188.h>
 #include <Adafruit_NeoPixel.h>
 #include "TrainerPanel.h"
+
+// Reset Pin is used for I2C or SPI
+#define CAP1188_RESET  9
+
+// CS pin is used for software or hardware SPI
+#define CAP1188_CS  10
+#define CAP1188_CS2  8
+
+// These are defined for software SPI, for hardware SPI, check your 
+// board's SPI pins in the Arduino documentation
+#define CAP1188_MOSI  11
+#define CAP1188_MISO  12
+#define CAP1188_CLK  13
+
+Adafruit_CAP1188 cap0 = Adafruit_CAP1188(CAP1188_CS, CAP1188_RESET);
+Adafruit_CAP1188 cap1 = Adafruit_CAP1188(CAP1188_CS2, CAP1188_RESET);
+
+uint16_t lastTouched1 = 0;
+uint16_t lastTouched2 = 0;
 
 #ifndef _BV
 #define _BV(bit) (1 << (bit))
@@ -32,17 +53,17 @@
 
 // ── Pin definitions ──────────────────────────────────────────────────────────
 #define LED_PIN         6          // LED data pin
-#define NUM_PADS        4
+#define NUM_PADS        16
 #define LEDS_PER_PAD    7
 
 // ── NeoPixel setup ───────────────────────────────────────────────────────────
 Adafruit_NeoPixel led_array(NUM_PADS * LEDS_PER_PAD, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // ── MPR121 setup ─────────────────────────────────────────────────────────────
-Adafruit_MPR121 cap0 = Adafruit_MPR121();
+//Adafruit_MPR121 cap0 = Adafruit_MPR121();
 //Adafruit_MPR121 cap1 = Adafruit_MPR121();
 
-#define PADS_PER_CAP_CONTROLLER 12
+#define PADS_PER_CAP_CONTROLLER 8
 #define TOUCH_THRESHOLD 16
 #define RELEASE_THRESHOLD 10
 
@@ -88,6 +109,7 @@ void setup() {
   led_array.setPixelColor(0, led_array.Color(255, 255, 255));
   led_array.show();
 
+  /*
   // MPR121 board 0 init
   if (!cap0.begin(0x5A)) {
     Serial.println(F("ERROR MPR121 board 0 not found"));
@@ -96,6 +118,7 @@ void setup() {
   cap0.setAutoconfig(true);
   cap0.setThresholds(TOUCH_THRESHOLD, RELEASE_THRESHOLD);
   Serial.println("MPR121 board 0 found and Initialized");
+  */
 
   // MPR121 board 1 init
   /*
@@ -107,6 +130,21 @@ void setup() {
   cap1.setThresholds(TOUCH_THRESHOLD, RELEASE_THRESHOLD);
   Serial.println("MPR121 board 1 found and Initialized");
   */
+
+  // CAP1188 setup
+  if (!cap0.begin()) {
+    Serial.println("CAP1188 1 not found");
+    while (1);
+  }
+  Serial.println("CAP1188 1 found!");
+  cap0.writeRegister(CAP1188_LEDLINK, 0);
+
+  if (!cap1.begin()) {
+    Serial.println("CAP1188 2 not found");
+    while (1);
+  }
+  Serial.println("CAP1188 2 found!");
+  cap1.writeRegister(CAP1188_LEDLINK, 0);
 
   Serial.println(F("CONTROLLER_READY"));
 }
@@ -141,7 +179,6 @@ void loop() {
       // reset our state
       cap0LastTouch = touch;
     }
-    /*
     touch = cap1.touched();
     if (touch != cap1LastTouch) {
       for (uint8_t i=0; i<12; i++) {
@@ -159,7 +196,6 @@ void loop() {
       // reset our state
       cap1LastTouch = touch;
     }
-    */
   }
   delay(5);
 }
